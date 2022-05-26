2e4153b23056fd2f1e8f8c1d0792dada9dd8b474
Saurabh Mehta <m.saurabh996@gmail.com>
2022-05-26 15:34:48 +0530

    connected frontend to back end and created data-base

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const generateToken = require('../utils/generateToken');
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password, pic} = req.body;
@@ -23,6 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
         email: user.email,
         isAdmin: user.isAdmin,
         pic: user.pic,
+        token: generateToken(user._id)
       });
     } else {
       res.status(400);
@@ -44,6 +46,7 @@ const authUser = asyncHandler(async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       pic: user.pic,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
